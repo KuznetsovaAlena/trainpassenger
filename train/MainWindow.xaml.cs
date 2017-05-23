763a9e8a6f24fbf9becd5fb06b99ef143d84b112
Алёна Кузнецова <alyona.kuz-17@list.ru>
2017-05-24 00:31:58 +0400

    new classes, show function

--- train/MainWindow.xaml.cs
+++ train/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +26,76 @@ namespace train
         {
             InitializeComponent();
         }
+        List<Passenger> Pass = new List<Passenger>();
+        List<Passenger> Passengers = new List<Passenger>();
+        private void show_Click(object sender, RoutedEventArgs e)
+        {
+           
+            foreach (Passenger p in Passengers)
+            {
+                if ((p.Departure == departure.Text)&(p.Arrival == arrival.Text)&(p.Date == date.Text)&(p.Numtrain==number.Text))
+                {
+                    Pass.Add(p);
+                }
+            }
+
+            info.ItemsSource = Pass;
+            
+            
+        }
+
+
+        private void conf_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            using (StreamReader sr = new StreamReader("base.txt"))
+                while (!sr.EndOfStream)
+                {
+
+                    string line = sr.ReadLine();
+
+                    string surname = line.Split('/')[0];
+                    string name = line.Split('/')[1];
+                    string patro = line.Split('/')[2];
+                    string date = line.Split('/')[3];
+                    string numtrain = line.Split('/')[4];
+                    uint numcar = uint.Parse(line.Split('/')[5]);
+                    uint seat = uint.Parse(line.Split('/')[6]);
+                    string departure = line.Split('/')[7];
+                    string arrival = line.Split('/')[8];
+
+                    Passengers.Add(new Passenger(surname, name, patro, date, numtrain, numcar, seat, departure, arrival));
+                }
+            
+            List<string> Train_number = new List<string>();
+
+            foreach (Passenger p in Passengers)
+            {
+                if (p.Departure == departure.Text)
+                {
+                    if (p.Arrival == arrival.Text)
+                    {
+                        if (p.Date == date.Text)
+                        {
+                            Train_number.Add(p.Numtrain);
+                        }
+                    }
+                    }
+                }
+            if (Train_number.Count==0)
+
+            {
+
+                MessageBox.Show("Такого поезда нет!");
+            }
+            for (int i = 0; i < Train_number.Count; i++)
+            {
+                number.Items.Add(Train_number[i]);
+            }
+            
+            }
+        }
     }
-}
+    
+
---
+++ train/Passenger.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace train
+{
+    class Passenger
+    {
+        private string surname;
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string patro;
+
+        public string Patro
+        {
+            get { return patro; }
+            set { patro = value; }
+        }
+
+        private string date;
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private string numtrain;
+
+        public string Numtrain
+        {
+            get { return numtrain; }
+            set { numtrain = value; }
+        }
+
+        private uint numcar;
+
+        public uint Numcar
+        {
+            get { return numcar; }
+            set { numcar = value; }
+        }
+
+        private uint seat;
+
+        public uint Seat
+        {
+            get { return seat; }
+            set { seat = value; }
+        }
+
+        private string departure;
+
+        public string Departure
+        {
+            get { return departure; }
+            set { departure = value; }
+        }
+
+        private string arrival;
+
+        public string Arrival
+        {
+            get { return arrival; }
+            set { arrival = value; }
+        }
+
+        public Passenger(string _surname, string _name, string _patro, string _date, string _numtrain, uint _numcar, uint _seat, string _departure, string _arrival)
+        {
+            surname = _surname;
+            name = _name;
+            patro = _patro;
+            date = _date;
+            numtrain = _numtrain;
+            numcar = _numcar;
+            seat = _seat;
+            departure = _departure;
+            arrival = _arrival;
+        }
+    }
+}
